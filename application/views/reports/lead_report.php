7719726a03891781c8bc26cfe5617541564cfeb1
shiva prasad <shiva@secondsdigital.com>
2020-04-30 16:24:02 +0530

    https

--- application/views/reports/lead_report.php
+++ application/views/reports/lead_report.php
@@ -181,7 +181,7 @@
              
              $.ajax({
                  "Type":"GET",
-                  "url":'http://'+window.location.host+'/admin/lead_report?user_id='+userid+'&fromDate='+fromDate+'&toDate='+toDate,
+                  "url":' https://'+window.location.host+'/admin/lead_report?user_id='+userid+'&fromDate='+fromDate+'&toDate='+toDate,
                   success: function (response) {
                       var data=JSON.parse(response)
                      var thead= Object.keys(data[0])
@@ -219,7 +219,7 @@
              var theadtr='';
                 $.ajax({
                  "Type":"GET",
-                 "url":'http://'+window.location.host+'/admin/lead_report?user_id='+user_id+'&fromDate='+fromDate+'&toDate='+toDate+'&lead_source_id='+lead_source_id,
+                 "url":' https://'+window.location.host+'/admin/lead_report?user_id='+user_id+'&fromDate='+fromDate+'&toDate='+toDate+'&lead_source_id='+lead_source_id,
                   success: function (response) {
                       var data=JSON.parse(response)
                       console.log(data)
@@ -231,7 +231,7 @@
                     //  theadtr+='<tr><th colspan=2>'+data[0].user_name+'</th></tr><tr><th>Count</th><th>Project Name</th></tr>';
                     //   $("#product-theadtr").append(theadtr);
                     for(i=0;i<=data.length-1;i++){
-                    	var url_of_project = 'http://'+window.location.host+'/admin/lead_report?user_id='+user_id+'&fromDate='+fromDate+'&toDate='+toDate+'&lead_source_id='+lead_source_id+'&project_id='+data[i].project_id;
+                    	var url_of_project = ' https://'+window.location.host+'/admin/lead_report?user_id='+user_id+'&fromDate='+fromDate+'&toDate='+toDate+'&lead_source_id='+lead_source_id+'&project_id='+data[i].project_id;
                     tbody +='<tr><td style="display:none;">'+data[i].project_id+'</td><td style="display:none;">'+data[i].user_id+'</td><td class="target"><a href="'+url_of_project+'"  target="_blank" style="cursor: pointer;">'+data[i].count+'</a></td><td style="display:none;">'+lead_source_id+'</td><td style="display:none;">'+fromDate+'</td><td style="display:none;">'+toDate+'</td><td>'+data[i].project+'</td></tr>'
                 }
                 $("#product_detail").append(tbody)
@@ -374,7 +374,7 @@
 
 									 <!--footer section start-->
 										<footer>
-										   <p>&copy <?= date('Y')?> Fullbasket Property . All Rights Reserved | Design by <a href="http://secondsdigital.com/" target="_blank">Seconds Digital Solutions.</a></p>
+										   <p>&copy <?= date('Y')?> Fullbasket Property . All Rights Reserved | Design by <a href=" https://secondsdigital.com/" target="_blank">Seconds Digital Solutions.</a></p>
 										</footer>
 									<!--footer section end-->
 								</div>
