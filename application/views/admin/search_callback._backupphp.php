459c2f7036a7aace846a79a9b9a2e65f9799ddae
shiva prasad <shiva@secondsdigital.com>
2020-07-18 09:13:56 +0530

    extra script code has been removed

--- application/views/admin/search_callback._backupphp.php
+++ application/views/admin/search_callback._backupphp.php
@@ -1172,5 +1172,293 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 <!-- Bootstrap Core JavaScript -->
    
+   <script>
+    $(document).ready(function() {
+         $('#example').DataTable({
+              "paging":   false,
+              "info": false
+ 
+        });
+        if (!Modernizr.inputtypes.date) {
+            // If not native HTML5 support, fallback to jQuery datePicker
+            $('input[type=date]').datepicker({
+                // Consistent format with the HTML5 picker
+                    dateFormat : 'dd/mm/yy'
+                }
+            );
+        }
+        if (!Modernizr.inputtypes.time) {
+            // If not native HTML5 support, fallback to jQuery timepicker
+            $('input[type=time]').timepicker({ 'timeFormat': 'H:i' });
+        }
+        $('#revenueMonth').MonthPicker({
+            Button: false
+        });
+        get_revenues();
+
+        $('.view_callbacks').click(function(){
+            var type = $(this).data('type');
+            var data = {};
+            switch (type)
+            {
+                case "user_total":
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.due_date = "<?php echo date('Y-m-d'); ?>";
+                    data.access = 'read_write'; 
+                    break;
+
+                case "user_overdue":
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.due_date_to = "<?php echo date('Y-m-d H:i:s'); ?>";
+                    data.for = "dashboard";
+                    data.access = 'read_write'; 
+                    break;
+
+                case "user_active": 
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.for = "dashboard";
+                    data.access = 'read_write'; 
+                    break;
+
+                case "user_close": 
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.status = "close";
+                    break;
+
+                case "user_important":
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.access = 'read_write'; 
+                    data.important = 1;
+                    break;
+
+                case "manager_active": 
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.for = "dashboard";
+                    data.access = 'read_write'; 
+                    break;
+
+                case "manager_close":
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.status = "close";
+                    break;
+            }
+            
+            view_callbacks(data,'post');
+
+        });
+
+        $("#refresh").click(function(){
+            $(".se-pre-con").show();
+            $.get("<?php echo base_url(); ?>dashboard/get_live_feed_back", function(response){
+                $("#live_feed_back_body").html(response);
+                $(".se-pre-con").hide("slow");
+            });
+        });
+
+        $("#overdue_lead_count").click(function(){
+            var form = document.createElement('form');
+            form.method = "POST";
+            form.action = "<?php echo base_url()."dashboard/generate_report" ?>";
+            
+            var input = document.createElement('input');
+            input.type = "text";
+            input.name = "toDate";
+            input.value = $(this).data('datetime');
+            form.appendChild(input);
+
+            input = document.createElement('input');
+            input.type = "text";
+            input.name = "reportType";
+            input.value = "due";
+            form.appendChild(input);
+
+            document.body.appendChild(form);
+            form.submit();
+        });
+
+        $('.emailSiteVisit').on('click', function(){
+            $(".se-pre-con").show();
+            $.ajax({
+                type : 'POST',
+                url : "<?= base_url('site-visit-report-mail');?>",
+                data:1,
+                success: function(res){
+                    $(".se-pre-con").hide("slow");
+                    if(res == 1)
+                        alert('Email Sent Successfully.');
+                    else
+                        alert('Email Sent fail!');
+                }
+            });
+        });
+
+    });
+    // $('#filter_revenue').click(get_revenues());
+    function get_revenues(){
+        $.get( "<?php echo base_url()."dashboard/get_revenue/" ?>"+$('#revenueMonth').val(), function( data ) {
+            $('#revenue_data').html(data);
+        });
+    }
+    function view_callbacks(data, method) {
+        var form = document.createElement('form');
+        form.method = method;
+        form.action = "<?php echo base_url()."view_callbacks?" ?>"+jQuery.param(data);
+        for (var i in data) {
+            var input = document.createElement('input');
+            input.type = "text";
+            input.name = i;
+            input.value = data[i];
+            form.appendChild(input);
+        }
+        //console.log(form);
+        document.body.appendChild(form);
+        form.submit();
+    }
+
+</script>
+<script>
+    $(document).ready(function() {
+         $('#example').DataTable({
+              "paging":   false,
+              "info": false
+ 
+        });
+        if (!Modernizr.inputtypes.date) {
+            // If not native HTML5 support, fallback to jQuery datePicker
+            $('input[type=date]').datepicker({
+                // Consistent format with the HTML5 picker
+                    dateFormat : 'dd/mm/yy'
+                }
+            );
+        }
+        if (!Modernizr.inputtypes.time) {
+            // If not native HTML5 support, fallback to jQuery timepicker
+            $('input[type=time]').timepicker({ 'timeFormat': 'H:i' });
+        }
+        $('#revenueMonth').MonthPicker({
+            Button: false
+        });
+        get_revenues();
+
+        $('.view_callbacks').click(function(){
+            var type = $(this).data('type');
+            var data = {};
+            switch (type)
+            {
+                case "user_total":
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.due_date = "<?php echo date('Y-m-d'); ?>";
+                    data.access = 'read_write'; 
+                    break;
+
+                case "user_overdue":
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.due_date_to = "<?php echo date('Y-m-d H:i:s'); ?>";
+                    data.for = "dashboard";
+                    data.access = 'read_write'; 
+                    break;
+
+                case "user_active": 
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.for = "dashboard";
+                    data.access = 'read_write'; 
+                    break;
+
+                case "user_close": 
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.status = "close";
+                    break;
+
+                case "user_important":
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.access = 'read_write'; 
+                    data.important = 1;
+                    break;
+
+                case "manager_active": 
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.for = "dashboard";
+                    data.access = 'read_write'; 
+                    break;
+
+                case "manager_close":
+                    data.advisor = "<?php echo $user_id; ?>";
+                    data.status = "close";
+                    break;
+            }
+            
+            view_callbacks(data,'post');
+
+        });
+
+        $("#refresh").click(function(){
+            $(".se-pre-con").show();
+            $.get("<?php echo base_url(); ?>dashboard/get_live_feed_back", function(response){
+                $("#live_feed_back_body").html(response);
+                $(".se-pre-con").hide("slow");
+            });
+        });
+
+        $("#overdue_lead_count").click(function(){
+            var form = document.createElement('form');
+            form.method = "POST";
+            form.action = "<?php echo base_url()."dashboard/generate_report" ?>";
+            
+            var input = document.createElement('input');
+            input.type = "text";
+            input.name = "toDate";
+            input.value = $(this).data('datetime');
+            form.appendChild(input);
+
+            input = document.createElement('input');
+            input.type = "text";
+            input.name = "reportType";
+            input.value = "due";
+            form.appendChild(input);
+
+            document.body.appendChild(form);
+            form.submit();
+        });
+
+        $('.emailSiteVisit').on('click', function(){
+            $(".se-pre-con").show();
+            $.ajax({
+                type : 'POST',
+                url : "<?= base_url('site-visit-report-mail');?>",
+                data:1,
+                success: function(res){
+                    $(".se-pre-con").hide("slow");
+                    if(res == 1)
+                        alert('Email Sent Successfully.');
+                    else
+                        alert('Email Sent fail!');
+                }
+            });
+        });
+
+    });
+    // $('#filter_revenue').click(get_revenues());
+    function get_revenues(){
+        $.get( "<?php echo base_url()."dashboard/get_revenue/" ?>"+$('#revenueMonth').val(), function( data ) {
+            $('#revenue_data').html(data);
+        });
+    }
+    function view_callbacks(data, method) {
+        var form = document.createElement('form');
+        form.method = method;
+        form.action = "<?php echo base_url()."view_callbacks?" ?>"+jQuery.param(data);
+        for (var i in data) {
+            var input = document.createElement('input');
+            input.type = "text";
+            input.name = i;
+            input.value = data[i];
+            form.appendChild(input);
+        }
+        //console.log(form);
+        document.body.appendChild(form);
+        form.submit();
+    }
+
+</script>
 </body>
 </html>
